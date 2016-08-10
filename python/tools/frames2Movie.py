bbeee0795a64b93630ac9cf96edbc24e7444c571
Mark Philip Philipsen <markpp@gmail.com>
2016-08-10 13:24:35 +0200

    data tools

--- python/tools/frames2Movie.py
+++ python/tools/frames2Movie.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import argparse
 import numpy as np
 from numpy import genfromtxt, savetxt
 import csv
@@ -7,26 +8,34 @@ import cv2
 import scipy
 import sys
 
+def mylistdir(directory):
+    """A specialized version of os.listdir() that ignores files that
+    start with a leading period."""
+    filelist = os.listdir(directory)
+    return [x for x in filelist
+            if not (x.startswith('.'))]
+
 if __name__ == "__main__":
-  path = sys.argv[1]
-  #path = '/home/markpp/Desktop/dataMTB/input/2015-09-02-12-44/'
-  #files = glob.glob("/media/markpp/WD1TBNTFS/MTBdata/2015-09-02-12-44(27Sep)/*.png")
-  #print files[0]
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-p", "--path", type=str,
+                  help="Path to frames")
+  ap.add_argument("-v", "--video", type=str, default="../../data/new_vid.avi",
+                  help="(optional) name of output video file")
+  args = vars(ap.parse_args())
+
+  path = args["path"]
+
   imagePath = ""
-  #fourcc = cv2.cv.CV_FOURCC('M','J','P','G')
   fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-  videoOut = cv2.VideoWriter('2015-09-30-06-23.avi',fourcc, 9, (80,60), False)
-  #print len(os.listdir(path))
-  imageCount = len(os.listdir(path))
+  #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+  #fourcc = cv2.VideoWriter_fourcc('H', '2', '6', '4')
+  videoOut = cv2.VideoWriter(args["video"],fourcc, 9, (80,60), False)
+  imageCount = len(mylistdir(path))
+  print imageCount
   for frame in range(0,imageCount):
-    filename = 'frame_' + str(frame).zfill(6) + '.png'
+    filename = '/frame_' + str(frame).zfill(6) + '.png'
     imagePath = path + filename
-    #print imagePath
     img = cv2.imread(imagePath,-1)
-    #if(img.size == 0):
-      #break
-    #img = (img/4).astype(np.uint8)
-    #np.bitwise_and(img, 1023)
     if(hasattr(img, 'astype')):
       oldImg = img    
     else:
@@ -34,17 +43,15 @@ if __name__ == "__main__":
       print imagePath
       if(frame < imageCount):
         img = oldImg
-        cv2.imwrite(imagePath, img)
+        #cv2.imwrite(imagePath, img)
       else:
         break
-
     img = np.clip(img, 0, 8191)
     img = (img/4).astype(np.uint8)
-    #cv2.imshow('test', img)
     videoOut.write(img)
+    #cv2.imshow('test', img)
     #if cv2.waitKey(1) & 0xFF == ord('q'):
       #break
   
   videoOut.release()
-    #prefix, num = filename[:-4].split('_')
-    
+
--- python/tools/renumber.py
+++ python/tools/renumber.py
@@ -1,9 +1,41 @@
-import os, sys
-path = sys.argv[1]
-for filename in os.listdir(path):
-	num = os.path.splitext(filename)[0]
-	num = num.zfill(6)
-	new_filename = "frame_" + num + ".png"
-	print filename
-	os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
-	
+import glob
+import os
+import argparse
+import numpy as np
+from numpy import genfromtxt, savetxt
+import csv
+import cv2
+
+if __name__ == "__main__":
+    #python renumber.py -p /Users/markpp/Desktop/code/data/test -s 12 -e 18
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", type=str,
+                    help="Path to frames")
+    ap.add_argument("-s", "--start", type=int,
+                    help="start frame, ignores everything before")
+    ap.add_argument("-e", "--end", type=int,
+                    help="end frame, ignores everything after")
+    args = vars(ap.parse_args())
+
+    path = args["path"]
+    start_num = args["start"]
+    end_num = args["end"]
+
+    new_path = path.rsplit('/',1)[0]+"/new"
+
+    print new_path
+
+    for filename in os.listdir(path):
+        #print filename
+        #prefix, num = filename[:-4].split('_')
+        name, postfix = filename[:].split('.')
+        if (postfix == 'png'):
+            prefix, num =name[:].split('_')
+            if(int(num) >= start_num and int(num) <= end_num):
+                new_num = int(num)-start_num
+                print "copying frame: {}, now called: {}".format(num, str(new_num).zfill(6))
+                #os.remove(os.path.join(path, filename))
+                new_filename = "frame_" + str(new_num).zfill(6) + ".png"
+                os.rename(os.path.join(path, filename), os.path.join(new_path, new_filename))
+
